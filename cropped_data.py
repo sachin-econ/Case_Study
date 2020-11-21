ae5452e3118c334fa5ef487e923893f7f35822a6
sachin-econ <73045776+sachin-econ@users.noreply.github.com>
2020-11-21 19:36:29 +0530

    Update cropped_data.py

--- cropped_data.py
+++ cropped_data.py
@@ -1,8 +1,8 @@
-def raw_data():###data_download
+def raw_data():
     !wget https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/imdb_crop.tar
     !wget https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/wiki_crop.tar
 
-    ###unzip
     !tar -xf imdb_crop.tar
-    !tar -xf wiki_crop.tar###unzip
+    !tar -xf wiki_crop.tar
 
+raw_data()
