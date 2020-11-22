4d4eaf061dd04d870d6583d74bd7c519a4c428a4
sachin-econ <73045776+sachin-econ@users.noreply.github.com>
2020-11-22 19:57:31 +0530

    Update cropped_data.py

--- cropped_data.py
+++ cropped_data.py
@@ -1,8 +1,7 @@
-def raw_data():
-    !wget https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/imdb_crop.tar
-    !wget https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/wiki_crop.tar
+!wget https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/imdb_crop.tar
+!wget https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/wiki_crop.tar
 
-    !tar -xf imdb_crop.tar
-    !tar -xf wiki_crop.tar
+!tar -xf imdb_crop.tar
+!tar -xf wiki_crop.tar
 
-raw_data()
+
