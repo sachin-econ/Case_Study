1732d77c3bf02a7f1ce821a418ead3d8cd575c98
sachin-econ <73045776+sachin-econ@users.noreply.github.com>
2020-11-22 22:14:15 +0530

    Update cropped_data.py

--- cropped_data.py
+++ cropped_data.py
@@ -1,7 +1,15 @@
-!wget https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/imdb_crop.tar
-!wget https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/wiki_crop.tar
-
-!tar -xf imdb_crop.tar
-!tar -xf wiki_crop.tar
-
-
+try:
+  try:
+    !wget https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/imdb_crop.tar
+    !wget https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/wiki_crop.tar
+    print('Download Complete')
+  except:
+    print('Download Failed')
+  try:
+    !tar - xf wiki_crop.tar
+    !tar - xf imdb_crop.tar
+    print('Extarction Complete')
+  except:
+    print('Extarction Failed')
+except:
+  print('Process Failed')
